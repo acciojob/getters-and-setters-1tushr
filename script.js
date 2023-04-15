b857479fac8b7b941382c4f86536620d3c63869f
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-04-15 07:23:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 //complete this code
-class Person {}
+class Person {
 
-class Student extends Person {}
 
-class Teacher extends Person {}
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+
+
+get name (){
+	return this.name;
+}
+
+	set age(age){
+		if(age>0&&age<120){
+			this.age=age;
+			
+		}
+		else{
+			console.error("invalid age");
+			
+		}
+	}
+
+	
+}
+
+class Student extends Person {
+
+study(){
+	console.log(`${this.name} is studying`);
+}
+
+	
+}
+
+class Teacher extends Person {
+
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
